bf0873bec5f6e26da7f94453fa5a14f865ccd3ec
MuhamadRifai1311 <149478908+MuhamadRifai1311@users.noreply.github.com>
2026-02-18 17:01:40 +0700

    commit again

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,7 +2,7 @@ import Dashboard from "./index"
 import About from "./about"
 import Experience from "./Experience"
 import Projects from "./Projects"
-import Contact from "./contact"
+import Contact from "./Contact"
 export default function Home(){
     return (
         <>
